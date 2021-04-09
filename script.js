9c2ee7c2d69480d4ddad4bc2137cb3f158b1f827
george572 <g.khutiashvili@gmail.com>
2021-04-08 19:34:12 -0700

    fixes

--- script.js
+++ script.js
@@ -17,25 +17,30 @@ window.onmousemove = function () {
         pupil.style.top = ((mouseY/80) + "px");
     }
     else {
-        pupil.style.top = ((mouseY/ 40) +  "px");
+        pupil.style.top = ((mouseY / 20) +  "px");
         pupil.style.width = 23 +"px";
         pupil.style.height = 23 +"px";
     }
-        pupil.style.left = ((mouseX/50) +  "px");
-        badura.style.top = ((mouseY/140) + 10 + "px");
-        badura.style.left = ((mouseX/65)  + 10 + "px");
+    pupil.style.left = ((mouseX/50) +  "px");
+    badura.style.top = ((mouseY/50) + 10 + "px");
+    badura.style.left = ((mouseX/65)  + 10 + "px");
 }
 
 password.addEventListener("keydown", function () {
     closeEye.style.height = 52 + 'px';
+    badura.style.display = "none"
+
 
 })
-password.addEventListener("onblur", function () {
-    closeEye.style.height = 52 + 'px';
+password.addEventListener("blur", function () {
+    closeEye.style.height = 0 + 'px';
+    badura.style.display = "block"
+
 
 })
 window.addEventListener("click", function () {
     closeEye.style.height = 52 + 'px';
+
 
     setTimeout(function () {
         closeEye.style.height = 0 + 'px';
